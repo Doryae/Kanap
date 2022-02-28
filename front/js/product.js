e02b22c74233da0fd2834c223ce072ef3092f306
Dylan Taverniers <dylan.taverniers@gmail.com>
2022-02-28 19:35:12 +0100

    Index 2.0 + Product 1.0
    - La page Index est terminée, le lien renvois bien l'Id de l'item cliqué dans l'URL. Toute la mise en page à été faite, le code amélioré, et disposé en plusieurs fonctions afin de faciliter la lecture de chaque étape. Ajouts de commentaires pour expliquer le fonctionnement de chaque fonctions.
    
    - Page Produit commencée ET terminée (en majorité), get / id réussi, on obtient bien l'objet cliqué et uniquement celui la, ainsi que toutes les informations relative (ajoutée en js). La page est complète dans sa forme.
    
    - Points d'améliorations possible pour :
    Index -- En cas d'erreur du fetch API, affiché un message d'erreur directement sur la page et non pas en console.log.  -> Rapide à faire.
    
    Product -- Si une couleur n'est pas sélectionnée, rendre le bouton non clickable (bouton ajouter au panier) + chercher à indiquer les informations que le client doit rentrer (ex : tant qu'aucune valeur n'est attribuée à l'option, ajouter un message d'alerte en dessous : "Veuillez choisir une couleur".

---
+++ front/js/product.js
@@ -0,0 +1,50 @@
+let url = new URLSearchParams(window.location.search);
+
+fetch('http://localhost:3000/api/products/' + url.get("id")) //récupère les données uniquement de l'objet possédant une valeur égale à l'id transmit.
+    .then(function(data){
+        return data.json();
+    })
+    .then(function(data){
+        showImage(data);
+        showInfos(data);
+        colorsChoice(data);
+        
+    })
+    .catch(function(err){
+        console.log("Erreur lors du chargement : Le serveur ne répond pas");
+    })
+
+/* --------------------------------- 
+Create a balise img, then add the attribut src with the same value as the image from the product
+Then, append this balise as a child of div class="item__img"
+-----------------------------------*/
+
+const showImage = (data) => {
+    let createElement = document.createElement("img");
+    createElement.setAttribute("src", `${data.imageUrl}`);
+    document.querySelector("article > div.item__img").appendChild(createElement);
+}
+
+/* --------------------------------- 
+Search the elements by ID and add the text contained in the keys : Name, Price, Description.
+-----------------------------------*/
+
+const showInfos = (data) => {
+    document.getElementById("title").textContent = `${data.name}`;
+    document.getElementById("price").textContent = `${data.price}`;
+    document.getElementById("description").textContent = `${data.description}`;
+}
+
+/* --------------------------------- 
+Search for how many colors is possible for the product, than for each of them create a balise option, 
+add an attribute value that is equal to the colors in question. (defined by i as the index)
+-----------------------------------*/
+
+const colorsChoice = (data) => {
+    for(i = 0; i < data.colors.length; i++){
+        let createOption = document.createElement("option");
+        createOption.setAttribute("value", `${data.colors[i]}`);
+        createOption.textContent = `${data.colors[i]}`;
+        document.getElementById("colors").appendChild(createOption);       
+    }
+}
--- front/js/script.js
+++ front/js/script.js
@@ -1,54 +1,86 @@
-/* 
-Pour la page index.html : Retrouver les données des produits, ensuite, pour l'affichage :
---> Tout ce qui sera ajouter à la page se trouvera dans la section à l'id/index items.
-- Une balise 'a' doit contenir le lien vers la page produit (qui possédera une autre mise en page) et dont l'url doit être égale à l'id produit
-- ouverture d'une balise article
-- ajout de l'image en fonction du produit
-- un h3 avec le nom du produit
-- un p de description
-- fermeture balise article + fermeture balise a. 
-*/
 
 fetch("http://localhost:3000/api/products")
     .then(function(data){
         return data.json();
     })
     .then(function(data){
-        console.log(data);
-        let productElement;
+
         data.forEach(element => {
-            productElement = document.createElement("a");
-            document.getElementById("items").appendChild(productElement);
-
-            productElement = document.createElement("article");
-            let linkArray = document.querySelectorAll("section > a")
-
-            for(let link of linkArray){
-                link.setAttribute("href", "#");
-                link.appendChild(productElement);
-            }
-
-            productElement = document.createElement("img")
-            productElementSecond = document.createElement("h3")
-            productElementThird = document.createElement("p")
-            let articleArray = document.querySelectorAll("article");
-
-            for(let article of articleArray){
-                article.appendChild(productElement)
-                productElement.setAttribute("src", "")
-                article.appendChild(productElementSecond)
-                productElementSecond.classList.add("productName")
-                article.appendChild(productElementThird)
-                productElementThird.classList.add("productDescription")
-            }
-
+            createIndexPagination();
         });
+        
+        getContent(data);
+
+        const linkArray = Array.from(document.querySelectorAll("section > a"));
+        // Va attribuer un lien allant chercher l'id du produit et l'ajouter à l'url product.html?id=. 
+        for(i = 0; i < linkArray.length; i++){
+            linkArray[i].setAttribute("href", `product.html?id=${data[i]._id}`);
+        }
     })
     .catch(function(err){
         console.log("Erreur lors du chargement : Le serveur ne répond pas");
     })
 
-// for(let produit of products){
-//     let html;
-// }
 
+/* --------------------------------- 
+Create the pagination of each products that is displayed on the index.html. 
+I try not to use InnerHTML, deciding to go with createElement.
+The structure of html for the products looks like : 
+<a href="..."><article><img src="url" alt="altText"><h3 class="productName">Name</h3><p class="productDescription">description</p></article></a>
+wich means : the balise a has a child : article
+            -> Article has three child : img, h3 and p. 
+            -> We need to add the attributes of some elements. for exemple : a need to have as an attribute : href
+            -> Same goes for the class of the h3 and the p
+
+/!\ That function only create the pagination of the pages. The links does not have an url, the images are not implemented, etc... 
+-----------------------------------*/
+
+const createIndexPagination = () => {
+    let productElement;
+
+    productElement = document.createElement("a");
+    document.getElementById("items").appendChild(productElement);
+
+    productElement = document.createElement("article");
+    const linkArray = document.querySelectorAll("section > a");
+
+    for(let link of linkArray){
+        link.appendChild(productElement);
+    }
+
+    productElement = document.createElement("img");
+    productElementSecond = document.createElement("h3");
+    productElementThird = document.createElement("p");
+    const articleArray = document.querySelectorAll("article");
+
+    for(let article of articleArray){
+        article.appendChild(productElement);
+        article.appendChild(productElementSecond);
+        productElementSecond.classList.add("productName");
+        article.appendChild(productElementThird);
+        productElementThird.classList.add("productDescription");
+    }
+};
+
+/* --------------------------------- 
+Get ALL the balise : img / h3 / p ; that have for parent element a balise article in the document. 
+The Array.from() change the nodelist due to the querySelectorAll into an array. 
+The boucle for will then add for each element (img / h3 / p) what they are lacking : attribute for images and text content for the other two. 
+They will use the i parameter as an index to obtain the correct information to be transfered, 
+then will iterate and continue until it catch up to the length of data. 
+Thus explaining why we need the data parameter for the function.
+-----------------------------------*/
+
+const getContent = (data) => {
+    const imgArray = Array.from(document.querySelectorAll("article > img"));
+    const titleArray = Array.from(document.querySelectorAll("article > h3"));
+    const descriptionArray = Array.from(document.querySelectorAll("article > p"));
+
+
+    for(i = 0; i < data.length; i++){
+        imgArray[i].setAttribute("src", `${data[i].imageUrl}`);
+        imgArray[i].setAttribute("alt", `${data[i].altTxt}`);
+        titleArray[i].textContent = `${data[i].name}`;
+        descriptionArray[i].textContent = `${data[i].description}`;
+    }
+}
