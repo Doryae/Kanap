602362803c0f07acd6c9ed1daad188c1a205ed67
Dylan Taverniers <dylan.taverniers@gmail.com>
2022-03-05 12:13:58 +0100

    Test 1.1 => Ajout function
    La fonction qui bug.

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,6 +3,16 @@ function saveAddedProduct() {
     localStorage.setItem("Produit", JSON.stringify(this));
 }
 
+function getAddedProduct(id) {
+    const lol = fetch("http://localhost:3000/api/products" + id)
+    const data = JSON.parse(lol); 
+    if(!id) {
+        throw "error";
+    } else {
+        return data;
+    }
+}
+
 function addProduct(productAdded) {
     let cart = getAddedProduct(productAdded);
     cart.push(productAdded);
